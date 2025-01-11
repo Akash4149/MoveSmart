729caeb79067475b4c55e883253145da6de92b16
Akash yadav <159035390+Akash4149@users.noreply.github.com>
2025-01-11 13:55:26 +0530

    i commited the image in it and also changed some text

--- frontend/src/pages/Driver/DriverLogin.jsx
+++ frontend/src/pages/Driver/DriverLogin.jsx
@@ -18,15 +18,15 @@ function DriverLogin() {
       <div className="min-h-screen w-[60%] ">
         <img
           className="cover h-[98%] mt-2 w-[98%] ml-2 rounded-lg"
-          src="https://imgs.search.brave.com/-xegy4nQi5b7bNvmct4g_fkDI52SRVnu7rPQtf6Sw7M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTMx/NjQ4MDgxMC9waG90/by9jYXJzLXRyYXZl/bGluZy10cm91Z2gt/Y2l0eS1pbi10aGUt/ZXZlbmluZy5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9WHpa/QTFOX1NZaEZITWsy/a3pUVEhIRUhucnJ0/b2x1bXpNRDA1YzYx/bVNFST0"
+          src="https://images.pexels.com/photos/1426516/pexels-photo-1426516.jpeg"
           alt=""
         />
       </div>
       <div className="my-auto  h-96 w-[40%] mx-auto">
         <h1 className="text-center font-bold text-3xl font-sans">
-          Welcome To Move Online
+          Welcome To Move smart
         </h1>
-        <p className="text-center my-4">Login Before start driving</p>
+        <p className="text-center my-4">Enter details Before start driving</p>
         <form onSubmit={handleSubmit} className="flex w-[60%] mx-auto flex-col gap-4 my-10" action="">
           <TextField
             value={busno}
@@ -41,7 +41,7 @@ function DriverLogin() {
             onChange={(e) => setPassword(e.target.value)}
             type="password"
             id="outlined-basic"
-            label="Password"
+            label="Driver ID"
             variant="outlined"
           />
           <Button
@@ -49,7 +49,7 @@ function DriverLogin() {
             sx={{ paddingTop: "10px", marginTop: "20px" }}
             variant="contained"
           >
-            Login
+            Enter
           </Button>
         </form>
       </div>
